922b52fe4160d48c0c9a445971e2b11307b60f99
Sela Privette <selaprivette@yahoo.com>
2022-07-25 17:25:47 -0400

    setting up proper directories

---
+++ develop/models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+// **User**:
+
+// * `username`
+//   * String
+//   * Unique
+//   * Required
+//   * Trimmed
+
+// * `email`
+//   * String
+//   * Required
+//   * Unique
+//   * Must match a valid email address (look into Mongoose's matching validation)
+
+// * `thoughts`
+//   * Array of `_id` values referencing the `Thought` model
+
+// * `friends`
+//   * Array of `_id` values referencing the `User` model (self-reference)
+class User extends Model {}
+User.init(
+    {
+        id:{
+            type: DataTypes
+
+
+        }
+    }
+)
